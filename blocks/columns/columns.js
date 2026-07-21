760cd6d50ee639c0388d54bfcb651f9fb9d22460
savitaagre <sagre@adobe.com>
2026-07-21 11:36:20 +0530

    Changes column js file

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,36 +1,55 @@
-import { fetchPlaceholders } from '/scripts/placeholders.js';
+// import { fetchPlaceholders } from '/scripts/placeholders.js';
  
-export default async function decorate(block) {
+// export default async function decorate(block) {
 
-  const placeholders = await fetchPlaceholders();
+//   const placeholders = await fetchPlaceholders();
 
-  //console.warn('placeholders', placeholders);
- //console.warn('placeholders.testPage', placeholders.testpage);
-  const rows = [...block.children];
+//   //console.warn('placeholders', placeholders);
+//  //console.warn('placeholders.testPage', placeholders.testpage);
+//   const rows = [...block.children];
  
-  // Assume first row is text, second is image
+//   // Assume first row is text, second is image
 
-  const textCol = rows[0];
+//   const textCol = rows[0];
 
-  const imageCol = rows[1];
+//   const imageCol = rows[1];
  
-  // Replace the paragraph with a placeholder
+//   // Replace the paragraph with a placeholder
 
-  const p = textCol.querySelector('p');
+//   const p = textCol.querySelector('p');
 
-  if (p) {
+//   if (p) {
 
-    p.textContent = placeholders.testpage ?? 'Default text';
+//     p.textContent = placeholders.testpage ?? 'Default text';
 
-  }
+//   }
  
-  // Remove any extra columns
+//   // Remove any extra columns
 
-  rows.slice(2).forEach((row) => row.remove());
+//   rows.slice(2).forEach((row) => row.remove());
  
-  block.classList.remove(...[...block.classList].filter((c) => c.startsWith('columns-')));
+//   block.classList.remove(...[...block.classList].filter((c) => c.startsWith('columns-')));
 
-  block.classList.add('columns-2-cols');
+//   block.classList.add('columns-2-cols');
 
+// }
+
+export default function decorate(block) {
+  const cols = [...block.firstElementChild.children];
+  block.classList.add(`columns-${cols.length}-cols`);
+
+  // setup image columns
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((col) => {
+      const pic = col.querySelector('picture');
+      if (pic) {
+        const picWrapper = pic.closest('div');
+        if (picWrapper && picWrapper.children.length === 1) {
+          // picture is only content in column
+          picWrapper.classList.add('columns-img-col');
+        }
+      }
+    });
+  });
 }
  
